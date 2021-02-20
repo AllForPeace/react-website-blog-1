f70908be932caf3b86efb9a321433329ecb2a3a9
nischalm <nischalmohan95@gmail.com>
2021-02-20 20:04:56 +0530

    kicked ass got my css working right -for the navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,20 @@
 const Navbar = () => {
-    return ( 
-        <div className="navbar">
-            <nav>
-                <h2 id="nav-logo">Nischal's Blog</h2>
-                <ul>
-                    <li><a id="about" href="#">About</a></li>
-                    <li><a id="contact" href="#">Contact</a></li>
-                    <li><a id="blog" href="#">Blog</a></li>
-                </ul>
-                {/* <a href="#">Log in</a>
-                <a href="#">Sign up</a> */}
-            </nav>
+  return (
+    <div className="navbar">
+      <nav>
+        <h2 id="nav-logo">Nischal's Blog</h2>
+        <ul>
+          <li><a id="about" href="#">About</a></li>
+          <li><a id="contact" href="#">Contact</a></li>
+          <li><a id="blog" href="#">Blog</a></li>
+        </ul>  
+      </nav>
+      <div className="login">
+        <li><a id="log-in" href="#">Log in</a></li>
+          <li><a id="sign-up" href="#">Sign up</a></li>
         </div>
-     );
-}
- 
+    </div>
+  );
+};
+
 export default Navbar;
