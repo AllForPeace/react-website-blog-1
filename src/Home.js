8b0e0d295d5a56bd21edc73b03187028e884fe84
nischalm <nischalmohan95@gmail.com>
2021-02-20 15:47:01 +0530

    added date posted and author data

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 const Home = () => {
     const [blogs, setBlogs] = useState('');
 
+    //abstract this part out - custom hook
     useEffect(()=>{
         fetch('http://localhost:8000/blogs')
         .then(res => res.json())
@@ -23,10 +24,12 @@ const Home = () => {
             <h2>All Blogs</h2>
             {blogs && blogs.map((blog) => (
                <article className="single-blog" key = {blog.id}>
-                    <h2> { blog.title }</h2>
+                    <h2 className="title"> { blog.title.toUpperCase() }</h2>
+                    <p className="posted-on"><span className="article-meta-info">Posted on: { blog.posted }</span></p>
                     <p className="body">
                         {blog.body}
                     </p>
+                    <p className="author"><span className="article-meta-info">Written by: { blog.author }</span></p>
                     </article>
             )) }
         </div>
