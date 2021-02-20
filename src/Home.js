324648d3736c6f459cb67d741972fe7e22958d86
nischalm <nischalmohan95@gmail.com>
2021-02-20 13:42:12 +0530

    fetched blogs from db.json and dynamic displaying of blogs working!

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,34 @@
+import { useState, useEffect } from 'react'
+
 const Home = () => {
+    const [blogs, setBlogs] = useState('');
+
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+        .then(res => res.json())
+        .then(data => {
+            setBlogs(data);
+            console.log(blogs);
+            console.log("Blog data is set");
+        })
+
+        return () => {
+            console.log("Clean up function right here.")
+        }
+    }, []);
+
     return ( 
         <div className="homePage">
             <h1>Yo Whatsapp!</h1>
-            <h2>This is the homepage!</h2>
+            <h2>All Blogs</h2>
+            {blogs && blogs.map((blog) => (
+               <article className="single-blog" key = {blog.id}>
+                    <h2> { blog.title }</h2>
+                    <p className="body">
+                        {blog.body}
+                    </p>
+                    </article>
+            )) }
         </div>
      );
 }
