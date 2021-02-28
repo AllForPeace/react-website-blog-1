e3a2862a9a045b329f8f9a2733bd8f0b10e99008
nischalm <nischalmohan95@gmail.com>
2021-02-28 17:20:29 +0530

    add cards component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Cards from '../Cards';
 import Hero from '../Hero'
 
 function Home() {
     return (
         <>
             <Hero />
+            <Cards />
         </>
     );
 }
